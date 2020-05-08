8aae4ae7282e290c4e061da85880d98bacfd2e38
Grayson Udstrand <grayson.udstrand@thrivent.com>
2020-05-07 18:01:58 -0700

    Initial commit

---
+++ src/main/java/technology/rutabaga/optipizza/OptiPizzaApplication.java
@@ -0,0 +1,13 @@
+package technology.rutabaga.optipizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptiPizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptiPizzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/technology/rutabaga/optipizza/OptiPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package technology.rutabaga.optipizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptiPizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
